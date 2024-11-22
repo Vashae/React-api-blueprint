cec24508a7740e17f7ab8a12669dfc91ddd87598
Vashae <vashaeb2@gmail.com>
2024-11-22 15:16:41 -0500

    hitting api in react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+function App() {
+  
+  return (
+  <Router>
+    <div className="app">
+
+<Routes>
+  <Route path='/' element={<Home />}></Route>
+  <Route path=':id' element ={<Posts />}></Route>
+</Routes>
+</div>
+</Router> 
+);
+
+}
+
+export default App;
